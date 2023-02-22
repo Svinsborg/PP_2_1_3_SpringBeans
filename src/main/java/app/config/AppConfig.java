28e5be2b3f4601c42960efedc483459af6633906
Andy <viktorov.a.m@mail.ru>
2023-02-22 17:27:19 +0300

    Done!

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,29 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name="dog")
+    public Dog dogBean() {
+        return new Dog();
+    }
+    @Bean(name="cat")
+    public Cat catBean() {
+        return new Cat();
+    }
+
+    @Bean(name="timer")
+    public Timer timerBean() {
+        return new Timer();
+    }
+
+
 }
